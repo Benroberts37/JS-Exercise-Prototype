0d5ae6317add1a469fcd4f2316c0a7dee2f831ca
Benroberts37 <benrobsemail@gmail.com>
2022-01-14 16:17:28 -0700

    Pushing completed project

--- index.js
+++ index.js
@@ -39,14 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = []
+}
+Person.prototype.eat = function(edible) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(edible)
+  }
 }
 
+Person.prototype.poop = function() {
+  this.stomach = []
+}
 
-
-
-
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
+}
 
 
 /*
@@ -63,8 +73,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, mpg) {
+  this.model = model;
+  this.milesPerGallon = mpg;
+  this.tank = 0
+  this.odometer = 0
+}
+
+Car.prototype.fill = function(gallons) {
+this.tank = this.tank + gallons
 }
 
 
@@ -75,18 +92,23 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+ Person.call(this, name, age);
+ this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global binding - the "this" keyword refers to the global object when it is used in the global sense 
+  2. Implicit binding - this refers to the object to the left of the dot operator. refers to the object on which the function is called on
+  3. explicit binding - We tell javascript engine to set this to point to a certain value using call, apply, or bind. (call and apply can be used to invoke a function with a specific value for "this")
+  4. new binding - using the new keyword constructs a new object, and this points it. When a function is invoked as a constructor function using the "new" keyword, "this" points to the new object that's created 
 */
 
 
